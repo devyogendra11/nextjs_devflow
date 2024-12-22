a95ce7712afa451455ef825623c0c77d34d233b0
yogendrabhadouriya <devyogendra11@gmail.com>
2024-12-22 15:03:03 +0530

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-      <h1 className="text-violet-500 font-bold">Hello Yogendra</h1>
+      <h1 className="text-3xl font-bold text-violet-500">Hello Yogendra</h1>
   );
 }
