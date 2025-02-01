f3d71d1ac633223e40c3be93f3e45cd72668c672
yogendrabhadouriya <devyogendra11@gmail.com>
2025-02-01 22:10:35 +0530

    Setup Auth Layout,Social auth Login using Github and Google

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,25 @@
-export default function Home() {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
   return (
     <>
       <h1 className="h1-bold">Hello Yogendra</h1>
-      <h1 className="h1-bold font-space-grotesk">Hello Yogendra (Intern)</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={ async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN});
+        }}
+      >
+        <Button type="submit">Log Out</Button>
+      </form>
     </>
   );
 }
+
+export default Home;
